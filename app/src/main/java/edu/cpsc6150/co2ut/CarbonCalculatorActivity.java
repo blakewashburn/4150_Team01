a27c0aff8276ff3063568e10b78b6f547c800a32
Blake <Blakewashburn1@gmail.com>
2019-11-08 07:57:02 -0500

    set up some of the necessary activities and worked on welcome screen

---
+++ app/src/main/java/edu/cpsc6150/co2ut/CarbonCalculatorActivity.java
@@ -0,0 +1,14 @@
+package edu.cpsc6150.co2ut;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CarbonCalculatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carbon_calculator);
+    }
+}
---
+++ app/src/main/java/edu/cpsc6150/co2ut/ReportCardActivity.java
@@ -0,0 +1,14 @@
+package edu.cpsc6150.co2ut;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReportCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_card);
+    }
+}
