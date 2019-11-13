3214c7d2bec916ba3a5bf9ec7f345f28defed68d
Blake <Blakewashburn1@gmail.com>
2019-11-13 09:53:09 -0500

    set up XML for report card page and set listeners for the buttons on the homepage to facilitate redirection amoungst pages

--- app/src/main/java/edu/cpsc6150/co2ut/MainActivity.java
+++ app/src/main/java/edu/cpsc6150/co2ut/MainActivity.java
@@ -2,7 +2,10 @@ package edu.cpsc6150.co2ut;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button calcCarbonFootprint = findViewById(R. id.carbon_calc_button_homepage);
+        Button mealLog = findViewById(R.id.meal_log_button_homepage);
+        Button reportCard = findViewById(R.id.report_card_button_homepage);
+
+        calcCarbonFootprint.setOnClickListener(calcCarbonListener);
+        mealLog.setOnClickListener(mealLogListener);
+        reportCard.setOnClickListener(reportCardListener);
     }
+
+    // Carbon Footprint Calculator Listener
+    private View.OnClickListener calcCarbonListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View view){
+            startActivity(new Intent(MainActivity.this, CarbonCalculatorActivity.class));
+        }   //end onClick
+    };  //end aboutButtonClickListener
+
+    // Meal Log Listener
+    private View.OnClickListener mealLogListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View view){
+            startActivity(new Intent(MainActivity.this, MealLogActivity.class));
+        }   //end onClick
+    };  //end aboutButtonClickListener
+
+    // Location Based Report Card Listener
+    private View.OnClickListener reportCardListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View view){
+            startActivity(new Intent(MainActivity.this, ReportCardActivity.class));
+        }   //end onClick
+    };  //end aboutButtonClickListener
 }
