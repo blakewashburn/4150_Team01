b43e753ba572256a9bee2676671ed5ea249fb212
Blake <Blakewashburn1@gmail.com>
2019-11-27 15:51:21 -0500

    worked on recyclerView, have it working with temporary data

--- app/src/main/java/edu/cpsc6150/co2ut/MealLogFragment.java
+++ app/src/main/java/edu/cpsc6150/co2ut/MealLogFragment.java
@@ -3,9 +3,11 @@ package edu.cpsc6150.co2ut;
 import android.content.Context;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -13,7 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MealLogFragment extends Fragment {
-    private onMealSelectedListener listener;
+    private Context context;
 
     // Required empty public constructor
     public MealLogFragment() {}
@@ -22,35 +24,23 @@ public class MealLogFragment extends Fragment {
         return new MealLogFragment();
     }   //end newInstance method
 
-    public interface onMealSelectedListener {
-        void onMealSelected(Meal meal);
-    }
-
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof onMealSelectedListener) {
-            listener = (onMealSelectedListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement onMealSelectedListener");
-        }   //end if-else
     }   //end onAttach method
 
     @Override
     public void onDetach() {
         super.onDetach();
-        listener = null;
     }   //end onDetach method
 
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-
+        // Bring the meal log fragment to the view
         View view = inflater.inflate(R.layout.fragment_meal_log, container, false);
-        ArrayList<Meal> mealLog = Meal.createMealLog();
+        context = getActivity();
+        ArrayList<Meal> mealLog = Meal.createMealLog(context);
 
         // set up recycleview
         RecyclerView recyclerView = view.findViewById(R.id.meal_log_recycler_view);
@@ -59,29 +49,39 @@ public class MealLogFragment extends Fragment {
         // set up recycleview adapter
         MealAdapter adapter = new MealAdapter(mealLog);
         recyclerView.setAdapter(adapter);
+        Log.d("ONCREATEVIEW", "We returned a view baby");
         return view;
-    }   //end onCreateView
+    }   //end onCreateView method
 
-    private class MealHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    private class MealHolder extends RecyclerView.ViewHolder {
         private Meal currentMeal;
-        private TextView mealSelected;
+        private TextView mealDate;
+        private ImageView mealPictured;
+        private TextView mealType;
+        private TextView impactScore;
+        private int count = 0;
 
         public MealHolder(LayoutInflater inflater, ViewGroup parent) {
             super(inflater.inflate(R.layout.list_meals, parent, false));
-            itemView.setOnClickListener(this);
-            mealSelected = itemView.findViewById(R.id.list_meal_date);
-        }   // end CityHolder constructor
+            Log.d("MEALHOLDER", "about to create a mealholder");
+            mealPictured = itemView.findViewById(R.id.rv_meal_pictured);
+            mealDate = itemView.findViewById(R.id.rv_list_meal_date);
+            mealType = itemView.findViewById(R.id.rv_meal_type);
+            impactScore = itemView.findViewById(R.id.rv_impact_score);
+            Log.d("MEALHOLDER", "We created a mealholder yall");
+        }   // end MealHolder constructor
 
         public void bind(Meal meal) {
+            Log.d("BIND", "About to bind a meal");
             currentMeal = meal;
             // TODO: to.String(*with format*) for the below currentMeal.getDate()
-            mealSelected.setText(currentMeal.getDate());
+            mealDate.setText(currentMeal.getDate());
+            mealPictured.setImageDrawable(meal.getImage());
+            mealType.setText(meal.getMealType());
+            impactScore.setText(meal.getImpactScore());
+            count++;
+            Log.d("BIND", "We made it to count #:" + count);
         }   //end bind method
-
-        @Override
-        public void onClick(View view) {
-            listener.onMealSelected(currentMeal);
-        }   //end onClick method
     }   //end MealHolder Class
 
     private class MealAdapter extends RecyclerView.Adapter<MealHolder> {
@@ -89,22 +89,27 @@ public class MealLogFragment extends Fragment {
 
         public MealAdapter(List<Meal> myMeals) {
             meals = myMeals;
+            Log.d("MEALADAPTER", "We created a mealAdapter boys");
         }   //end MealAdapter method
 
         @Override
         public MealHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            Log.d("CREATEVIEWHOLDER", "about to create that view holder");
             LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
             return new MealHolder(layoutInflater, parent);
         }   //end onCreateViewHolder method
 
         @Override
         public void onBindViewHolder(MealHolder holder, int position) {
+            Log.d("BindViewHolder", "about to bind a view holder");
             Meal meal = meals.get(position);
             holder.bind(meal);
+            Log.d("BindViewHolder", "just bound that view holder");
         }   //end onBindViewHolder method
 
         @Override
         public int getItemCount() {
+            Log.d("getItemCount", "asking for item count size, returning "+ meals.size());
             return meals.size();
         }   //end getItemCount method
     }   //end MealAdapter class
